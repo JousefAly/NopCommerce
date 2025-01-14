6f92a03eb0a845144146d60e1625634eca531568
Yousef Aly <youssef.ali@arabiansystems.com>
2025-01-14 15:40:16 +0200

    make component work

---
+++ src/Plugins/Nop.Plugin.Widgets.SumNumbers/SumComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nop.Plugin.Widgets.SumNumbers.Models;
+using Nop.Web.Framework.Components;
+
+namespace Nop.Plugin.Widgets.SumNumbers.Components;
+
+[ViewComponent(Name = "SumNumbers")]
+public class SumComponent : NopViewComponent
+{
+
+    public async Task<IViewComponentResult> InvokeAsync(string widgetZone, object additionalData)
+    {
+        var model = new SumCalculatorModel();
+
+        return View("~/Plugins/Widgets.SumNumbers/Views/SumNumbers.cshtml", model);
+    }
+}
---
+++ src/Plugins/Nop.Plugin.Widgets.SumNumbers/SumNumbersPlugin.cs
@@ -0,0 +1,21 @@
+﻿using Nop.Plugin.Widgets.SumNumbers.Components;
+using Nop.Services.Cms;
+using Nop.Services.Plugins;
+using Nop.Web.Framework.Infrastructure;
+
+namespace Nop.Plugin.Widgets.SumNumbers;
+
+public class SumNumbersPlugin : BasePlugin, IWidgetPlugin
+{
+    public bool HideInWidgetList => false;
+
+    public Type GetWidgetViewComponent(string widgetZone)
+    {
+        return typeof(SumComponent);
+    }
+
+    public async Task<IList<string>> GetWidgetZonesAsync()
+    {
+        return new List<string> { PublicWidgetZones.HeaderAfter };
+    }
+}
