e7ccc6fb67f414c0bcdb233039db4122b4a747bb
Yousef Aly <youssef.ali@arabiansystems.com>
2025-02-04 10:29:47 +0200

    use stripe page

--- src/Nop.Plugin.Payments.StripeCustom/StripePaymentViewComponent.cs
+++ src/Nop.Plugin.Payments.StripeCustom/StripePaymentViewComponent.cs
@@ -15,8 +15,8 @@ public class StripePaymentViewComponent : ViewComponent
     {
         var model = new StripePaymentModel
         {
-            PublishableKey = _stripeSettings.PublishableKey,
-            ClientSecret = TempData["StripeClientSecret"]?.ToString()
+            PublishableKey = "pk_test_51QlpoxGIYp5gF2nJgK4rwrgW2aDiIinlBhiYhtgdnpD6M8vouHsExr3s81pWQgGECXjWCMSBZtimMwa9LwKMbz7H00lMkvmA1n",
+            ClientSecret = "sk_test_51QlpoxGIYp5gF2nJhM5rQ7sKwWsh8nGVQsxlLPy8esDRUMchXb9KnaoQcAALQFwyXRY8miaAa8bQkBo9BcFXsPkT00nWZMefXS"
         };
 
         return View("~/Plugins/Payments.StripeCustom/Views/PaymentInfo.cshtml", model);
